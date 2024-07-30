4bfbd19536eb8369423589d33f44ab5838dffad4
GabrielGNM <gabriel.gnm@gmail.com>
2024-07-29 23:03:22 -0300

    Add project files.

---
+++ Controllers/PacienteController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PacienteController : ControllerBase
+    {
+        private readonly ILogger<PacienteController> _logger;
+
+        public PacienteController(ILogger<PacienteController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult<HttpClient> BuscarTodosPacientes()
+        {
+            _logger.LogInformation("teste");
+            return Ok();
+        }
+    }
+}
